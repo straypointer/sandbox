6cc3d4c727bba92e83f0d542d519f1943d7882b7
Matt Galligan <matt.galligan@buildingstart.com>
2013-03-01 13:35:12 -0600

    working?

--- logcheck.pl
+++ logcheck.pl
@@ -6,12 +6,21 @@ use warnings;
 sub search {
 	# expects:
 	# filespec, search string, output file
+	my $source = shift;
 	my $spec = shift;
 	my $str = shift;
+	my $output = shift;
+	my $cmd;
 	
-	print "$spec \n";
-	print "$str \n";
+	# echo statement
+	$cmd = "\nTHE FOLLOWING CONTAINS A LIST OF LOGS WITH THE STRING \"$str\" - CASE SENSITIVE SEARCH\n";
 	
+	system("echo '$cmd'");
+	
+	# find statement
+	$cmd = "find $source -prune -name \"$spec\" -type f -print | xargs grep $str >> $output 2>/dev/null";
+
+	system($cmd);
 }
 
 # main program
@@ -19,16 +28,14 @@ sub search {
 my @txt_array = ("The status on", "is not valid", "ERROR", "Cannot open");
 my @log_array = ("WARNING: UNINITIALIZED VARIABLE", "CLIENTs CHECKED. Error count = 1", "WRONG ! TRY AGAIN", "MAXIMUM NUMBER OF PAGES HAS BEEN REACHED",
 				"FTPCount=", " Not owner", "spawn id exp");
+my $output_file = "/udadmin/expectfiles/print_a_report.txt";
+my $input_file = "/udadmin/expectlogs/*";
 
-
-search("*.log", "while executing");
-# wrong directory command
-search("*.txt", "The status on");
-
+				
 foreach (@txt_array) {
-	search("*.txt", "$_");
+	search($input_file, "*.txt", "$_", $output_file);
 }
 
 foreach (@log_array) {
-	search("*.log", "$_");
+	search($input_file, "*.log", "$_", $output_file);
 }
